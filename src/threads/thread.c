35ea3e1ebada605e00a09431e823362133d73e49
linzch3 <linzch3@outlook.com>
2017-04-13 19:15:52 +0800

    【无关紧要】强迫症删除了几行空格而已

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -184,10 +184,8 @@ thread_create (const char *name, int priority,
   init_thread (t, name, priority);
   tid = t->tid = allocate_tid ();
 
-
   /* Initialize the bolcked time of a thread. */
   t->ticks_blocked = 0;
-
 
   /* Prepare thread for first run by initializing its stack.
      Do this atomically so intermediate values for the 'stack' 
